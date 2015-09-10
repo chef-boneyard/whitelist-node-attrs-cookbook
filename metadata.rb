9b94b562d026a08f2bc3788b376f9c745c39a726
Tim Smith <tsmith84@gmail.com>
2015-09-09 20:45:26 -0700

    Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,10 @@
 name             "whitelist-node-attrs"
-maintainer       "Opscode, Inc"
-maintainer_email "cookbooks@opscode.com"
+maintainer       "Chef Software, Inc"
+maintainer_email "cookbooks@chef.io"
 license          "Apache 2.0"
 description      "Allows you to specify a whitelist of node attributes to save on the Chef Server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1.0"
+
+source_url 'https://github.com/chef-cookbooks/whitelist-node-attrs' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/whitelist-node-attrs/issues' if respond_to?(:issues_url)
